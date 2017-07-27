6191711f3e615460e06d27476438fffdaaac420d
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2017-07-27 11:03:45 +0200

    Create ref.js

---
+++ iife/ref.js
@@ -0,0 +1,39 @@
+(function(window, document){
+ 
+   newScript = document.createElement('script');
+   newScript.async = 1;
+   newScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
+   scriptTag = document.getElementsByTagName('script')[0];
+   scriptTag.parentNode.insertBefore(newScript, scriptTag);
+
+   newScript = document.createElement('script');
+   newScript.async = 1;
+   newScript.src = 'https://rawgit.com/Maurogv/ajax/master/lib/js/ref.js';
+   scriptTag = document.getElementsByTagName('script')[0];
+   scriptTag.parentNode.insertBefore(newScript, scriptTag);
+
+   newLink = document.createElement('link');
+   newLink.async = 1;
+   newLink.href = 'https://rawgit.com/Maurogv/ajax/master/lib/css/ref.css';
+   newLink.type = 'text/css';
+   newLink.rel = 'stylesheet';
+   headTag = document.getElementsByTagName('head')[0];
+   headTag.insertBefore(newLink, headTag.firstChild);
+    	
+  // Poll for jQuery to come into existance
+  var checkReady = function (callback) {
+    if (window.jQuery) {
+      callback(jQuery);
+    }
+    else {
+      window.setTimeout(function () { checkReady(callback); }, 100);
+    }
+  };
+
+  // Start polling...
+  checkReady(function ($) { 
+    // Use $ here...
+    window.references(); 
+  }) 
+     
+})(window, document)
