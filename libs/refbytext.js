33420627c4c50664bb9358762b51d30d46368fd6
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2018-01-20 11:28:21 +0100

    Create refbytext.js

---
+++ libs/refbytext.js
@@ -0,0 +1,20 @@
+(function (window) {
+
+    var references = function () {          
+        $("div:contains('[^]')").each(function (index) {
+            $(this).text("");
+            var a = $("a")
+            a.attr("href", "#cite_note-" + (index + 1) + "> [" + (index + 1) + "]");
+            a.attr("id", "cite_ref-" + (index + 1));       
+            $(this).append(a);
+        });
+        //  <li id="cite_note-1"><a href="#cite_ref-1"><b>^</b></a> Cite Note-1</li>
+        $(".references li").each(function (index) {
+            $(this).attr("id", "cite_note-" + (index + 1));
+            $(this).prepend("<a href=#cite_ref-" + (index + 1) + " class='text-decoration-none'><b>^</b></a> ");
+        });
+    }
+
+    window.references = references;
+
+})(window);
