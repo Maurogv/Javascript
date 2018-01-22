2187e508a0771e79ab3457f7ccfa244b4e033786
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2018-01-22 20:11:18 +0100

    Update refbytext.js

--- libs/refbytext.js
+++ libs/refbytext.js
@@ -1,14 +1,16 @@
 (function (window) {
 
     var references = function () {
+        index = 0;
         text = '[^]';
-        $("*:contains(" + text + ")").each(function (index) {
-            $(this).text("");
-            var a = $("a");
-            a.attr("href", "#cite_note-" + (index + 1) + "> [" + (index + 1) + "]");
-            a.attr("id", "cite_ref-" + (index + 1));       
-            $(this).append(a);
-            $(this).text().replace("contains", "hello everyone"); 
+        $("*:contains(" + text + ")").each(function () {                  
+            $(this).text().replace(text, function () {    
+                var a = $("a");
+                a.attr("href", "#cite_note-" + (index + 1) + "> [" + (index + 1) + "]");
+                a.attr("id", "cite_ref-" + (index + 1));  
+                index++;     
+                return a;
+            }); 
         });
         //  <li id="cite_note-1"><a href="#cite_ref-1"><b>^</b></a> Cite Note-1</li>
         $(".references li").each(function (index) {
