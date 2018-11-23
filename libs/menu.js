189973f8a6aa2705f758b9562bd173e28c590f4f
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2018-11-23 21:54:06 +0100

    Update menu.js

--- libs/menu.js
+++ libs/menu.js
@@ -5,7 +5,7 @@
         if ( $("*").has('h2').length > 0 )  tag = "h2";
          
         var div = $("<div></div>");
-        $("tag").each(function (index) {
+        $(tag).each(function (index) {
             $(this).attr("id", $(this).text().replace(' ', '_'));
             if (!div.is(':empty')) div.append(' | ');
             div.append("<a href=#" + $(this).text().replace(' ', '_') + ">" + $(this).text() + "</a>");
